036ef6958dbedfea0c56000cba5ac6fc43e52e09
Xuân Nghĩa <126942340+Xn0311@users.noreply.github.com>
2025-09-12 08:34:32 +0700

    lỗi 2

--- Vd2.cs
+++ Vd2.cs
@@ -9,11 +9,7 @@ using System;
 using System.Collections.Generic;
 
 public class Student
-{
-    public string Id { get; set; }
-    public string Name { get; set; }
-    public int Age { get; set; }
-    public double GPA { get; set; }
+{public string Id { get; set; }public string Name { get; set; }public int Age { get; set; }public double GPA { get; set; }
 }
 
 public class SchoolProgram
