12038bda2061cb86ce3f89b2c7de5b9702d17a1c
hoangan-110 <anvu4111@gmail.com>
2025-09-12 09:09:12 +0700

    Update Vd1.cs

--- Vd1.cs
+++ Vd1.cs
@@ -21,3 +21,36 @@ class Student {
         return id + " - " + name + " (" + major + ")";
     }
 }
+class Teacher {
+    private String id;
+    private String name;
+    private String department;
+
+    public Teacher(String id, String name, String department) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + name + " (" + department + ")";
+    }
+}
+
+class Course {
+    private String id;
+    private String title;
+    private int credit;
+
+    public Course(String id, String title, int credit) {
+        this.id = id;
+        this.title = title;
+        this.credit = credit;
+    }
+
+    @Override
+    public String toString() {
+        return id + " - " + title + " (" + credit + " tín chỉ)";
+    }
+}
